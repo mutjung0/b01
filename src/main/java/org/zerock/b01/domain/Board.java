cb27f6b642664965a63233e18b6fcef24e59676c
mutjung0 <mutjung0@gmail.com>
2024-11-11 17:45:15 +0900

    commit

--- src/main/java/org/zerock/b01/domain/Board.java
+++ src/main/java/org/zerock/b01/domain/Board.java
@@ -1,15 +1,18 @@
 package org.zerock.b01.domain;
 
 import lombok.*;
+import org.hibernate.annotations.BatchSize;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
+@ToString(exclude = "imageSet")
 public class Board extends BaseEntity {
 
     @Id
@@ -29,5 +32,27 @@ public class Board extends BaseEntity {
     public void change(String title, String content) {
         this.title = title;
         this.content = content;
+    }
+
+    @OneToMany(mappedBy = "board", cascade = {CascadeType.ALL}, fetch = FetchType.LAZY, orphanRemoval = true)
+    @Builder.Default
+    @BatchSize(size = 10)
+    private Set<BoardImage> imageSet = new HashSet<>();
+
+    public void addImage(String uuid, String fileName) {
+        BoardImage boardImage = BoardImage.builder()
+                .uuid(uuid)
+                .fileName(fileName)
+                .board(this)
+                .ord(imageSet.size())
+                .build();
+        imageSet.add(boardImage);
+    }
+
+    public void clearImages() {
+
+        imageSet.forEach(boardImage -> boardImage.changeBoard(null));
+
+        this.imageSet.clear();
     }
 }
--- src/test/java/org/zerock/b01/service/BoardServiceTests.java
+++ src/test/java/org/zerock/b01/service/BoardServiceTests.java
@@ -4,9 +4,11 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.zerock.b01.dto.BoardDTO;
-import org.zerock.b01.dto.PageRequestDTO;
-import org.zerock.b01.dto.PageResponseDTO;
+import org.zerock.b01.dto.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 @SpringBootTest
 @Log4j2
@@ -62,5 +64,95 @@ public class BoardServiceTests {
 
         PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
         log.info(responseDTO);
+    }
+
+    @Test
+    public void registerWithImages() {
+
+        log.info(boardService.getClass().getName());
+
+        BoardDTO boardDTO = BoardDTO.builder()
+                .title("File...Sample Title...")
+                .content("Sample Content...")
+                .writer("user00")
+                .build();
+
+        boardDTO.setFileNames(
+                Arrays.asList(
+                        UUID.randomUUID()+"_aaa.jpg",
+                        UUID.randomUUID()+"_bbb.jpg",
+                        UUID.randomUUID()+"_bbb.jpg"
+                ));
+
+        Long bno = boardService.register(boardDTO);
+
+        log.info("bno: " + bno);
+    }
+
+    @Test
+    public void readAll() {
+
+        Long bno = 101L;
+
+        BoardDTO boardDTO = boardService.readOne(bno);
+
+        log.info(boardDTO);
+
+        for (String fileName : boardDTO.getFileNames()) {
+            log.info(fileName);
+        }//end for
+
+    }
+
+    @Test
+    public void modifyImage() {
+
+        //변경에 필요한 데이터
+        BoardDTO boardDTO = BoardDTO.builder()
+                .bno(101L)
+                .title("Updated....101")
+                .content("Updated content 101...")
+                .build();
+
+        //첨부파일을 하나 추가
+        boardDTO.setFileNames(Arrays.asList(UUID.randomUUID()+"_zzz.jpg"));
+
+        boardService.modify(boardDTO);
+
+    }
+
+    @Test
+    public void removeAll() {
+
+        Long bno = 1L;
+
+        boardService.remove(bno);
+
+    }
+
+    @Test
+    public void listWithAll() {
+
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .build();
+
+        PageResponseDTO<BoardListAllDTO> responseDTO =
+                boardService.listWithAll(pageRequestDTO);
+
+        List<BoardListAllDTO> dtoList = responseDTO.getDtoList();
+
+        dtoList.forEach(boardListAllDTO -> {
+            log.info(boardListAllDTO.getBno()+":"+boardListAllDTO.getTitle());
+
+            if(boardListAllDTO.getBoardImages() != null) {
+                for (BoardImageDTO boardImage : boardListAllDTO.getBoardImages()) {
+                    log.info(boardImage);
+                }
+            }
+
+            log.info("-------------------------------");
+        });
     }
 }
